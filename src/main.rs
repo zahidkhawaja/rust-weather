c7e868bace9445206335b88402e57a09d7ce7ba7
Zahid Khawaja <zahidkhawaja@icloud.com>
2022-09-02 18:36:24 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,32 @@ extern crate openweathermap;
 use openweathermap::blocking::weather;
 
 fn main() {
+    // Default city used if no args passed
+    let default = String::from("Atlanta");
+
+    let mut city = String::new();
+
+    let mut arguments: Vec<String> = args().collect();
+    arguments.remove(0);
+
+    if arguments.len() == 0 {
+        city = default;
+    }
+
+    for x in arguments {
+        city.push_str(" ");
+        city.push_str(&x);
+    }
+
+    fetch_weather(&city);
+}
+
+fn fetch_weather(city: &str) {
+    // You can get an API key from https://openweathermap.org
     dotenv().ok();
     let api_key = std::env::var("API_KEY").expect("API_KEY not set");
 
-    let city: String;
-
-    match args().len() {
-        2 => city = args().nth(1).unwrap().to_string(),
-        3 => city = args().nth(1).unwrap().to_string() + " " + &args().nth(2).unwrap().to_string(),
-        4 => {
-            city = args().nth(1).unwrap().to_string()
-                + " "
-                + &args().nth(2).unwrap().to_string()
-                + " "
-                + &args().nth(3).unwrap().to_string()
-        }
-        _ => city = "Atlanta".to_string(),
-    }
-
-    // You can get an API key from https://openweathermap.org
-    match &weather(&city, "imperial", "en", &api_key) {
+    match &weather(city, "imperial", "en", &api_key) {
         Ok(current) => {
             println!(
                 "It is currently {} degrees Fahrenheit in {}!",
